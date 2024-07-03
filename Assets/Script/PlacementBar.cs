3787a9f6437ae8e75459209b6007e2b93adc3f5a
MZQ\15215 <15215217793@163.com>
2024-07-04 00:29:37 +0800

    位置对不准的问题

--- Assets/Script/PlacementBar.cs
+++ Assets/Script/PlacementBar.cs
@@ -6,6 +6,8 @@ using System.Linq;
 
 public class PlacementBar : MonoBehaviour
 {
+    private enum RemoveType { None ,Pung, Chow, Kong }
+
     [SerializeField] private int slugCount;
     [SerializeField] private GameObject model;
 
@@ -29,20 +31,38 @@ public class PlacementBar : MonoBehaviour
     private void Start() {
         modelSize = model.transform.GetComponent<Renderer>().bounds.size;
     }
-    private void Update() {
-        CheckToRemove();
-    }
     public void InsertToSlug(GameObject obj) {
         var mj = obj.GetComponent<Mahjong>();
         int count = mj.type == Mahjong.CombinationType.Single ? 1 : 2;
         if(count + placeCount > slugCount) return;
-        float LeftX = (float)((placeCount - 4) * modelSize.x);
+
+        mahjongs.Add(obj);
         obj.GetComponent<Rigidbody>().isKinematic = true;
         obj.GetComponent<BoxCollider>().enabled = false;
         obj.transform.parent = transform;
         obj.transform.DORotateQuaternion(Quaternion.identity, 0.3f);
-        obj.transform.DOLocalMove(new Vector3(LeftX + modelSize.x / 2* count, 0, 0), 1).OnComplete(()=> {
-            mahjongs.Add(obj);
+
+        float LeftX = (float)((placeCount - 4) * modelSize.x);
+
+        var (removeType, removeObjs) = CheckToRemove();
+        if(removeType == RemoveType.Chow || removeType == RemoveType.Pung) {
+            placeCount -= 3;
+        } else if(removeType == RemoveType.Kong) {
+            placeCount -= 4;
+        }
+        if(removeType != RemoveType.None) {
+            foreach(var item in removeObjs) {
+                mahjongs.Remove(item);
+            }
+        }
+
+        obj.transform.DOLocalMove(new Vector3(LeftX + modelSize.x / 2* count, 0, 0), 0.3f).OnComplete(()=> {
+            if(removeType != RemoveType.None) {
+                foreach(var item in removeObjs) {
+                    Destroy(item);
+                    ResetArrange();
+                }
+            }
         });
         placeCount += count;
     }
@@ -57,44 +77,45 @@ public class PlacementBar : MonoBehaviour
         return result;
     }
 
-    private void CheckToRemove() {
+    private (RemoveType, GameObject[]) CheckToRemove() {
         for(int i = 0; i < mahjongs.Count; i++) {
             for(int j = i + 1; j < mahjongs.Count; j++) {
                 var m1 = mahjongs[i];
                 var m2 = mahjongs[j];
-                if(CanRemove(m1, m2)) {
-                    mahjongs.Remove(m1);
-                    mahjongs.Remove(m2);
-                    Destroy(m1);
-                    Destroy(m2);
-                    placeCount = CalculatePlaceCount();
-                    ResetArrange();
-                    return;
+                var type = CanRemove(m1, m2);
+                if(type != RemoveType.None){ 
+                    return (type, new GameObject[] {m1, m2});
                 }
             }
         }
+        return (RemoveType.None, null);
     }
 
     private void ResetArrange() {
         int total = 0;
         foreach(var item in mahjongs) {
-             var mj = item.GetComponent<Mahjong>();
+            var mj = item.GetComponent<Mahjong>();
             int count = mj.type == Mahjong.CombinationType.Single ? 1 : 2;
             float LeftX = (float)((total - 4) * modelSize.x);
-            item.transform.DOLocalMove(new Vector3(LeftX + modelSize.x / 2* count, 0, 0), 0.5f);
+            item.transform.DOLocalMove(new Vector3(LeftX + modelSize.x / 2* count, 0, 0), 0.3f);
             total += count;
         }
     }
 
-    private bool CanRemove(GameObject o1, GameObject o2) {
+    private RemoveType CanRemove(GameObject o1, GameObject o2) {
         var mj1 = o1.GetComponent<Mahjong>();
         var mj2 = o2.GetComponent<Mahjong>();
-        if(mj1.category != mj2.category) return false;
-        if(mj1.type == Mahjong.CombinationType.Single && mj2.type == Mahjong.CombinationType.Pair) return mj1.num == mj2.num;
-        if(mj2.type == Mahjong.CombinationType.Single && mj1.type == Mahjong.CombinationType.Pair) return mj1.num == mj2.num;
-        if(mj2.type == Mahjong.CombinationType.Pair && mj1.type == Mahjong.CombinationType.Pair) return mj1.num == mj2.num;
-        if(mj1.type == Mahjong.CombinationType.Single && mj2.type == Mahjong.CombinationType.Run) return mj1.num + 1 == mj2.num || mj1.num - 2 == mj2.num;
-        if(mj2.type == Mahjong.CombinationType.Single && mj1.type == Mahjong.CombinationType.Run) return mj2.num + 1 == mj1.num || mj2.num - 2 == mj1.num;
-        return false;
+        if(mj1.category != mj2.category) return RemoveType.None;
+        // 碰
+        if(mj1.type == Mahjong.CombinationType.Single && mj2.type == Mahjong.CombinationType.Pair && mj1.num == mj2.num) return RemoveType.Pung;
+        if(mj2.type == Mahjong.CombinationType.Single && mj1.type == Mahjong.CombinationType.Pair && mj1.num == mj2.num) return RemoveType.Pung;
+        // 杠
+        if(mj2.type == Mahjong.CombinationType.Pair && mj1.type == Mahjong.CombinationType.Pair && mj1.num == mj2.num) return RemoveType.Kong;
+        // 吃
+        if(mj1.type == Mahjong.CombinationType.Single && mj2.type == Mahjong.CombinationType.Run && 
+           (mj1.num + 1 == mj2.num || mj1.num - 2 == mj2.num)) return RemoveType.Chow;
+        if(mj2.type == Mahjong.CombinationType.Single && mj1.type == Mahjong.CombinationType.Run && 
+          (mj2.num + 1 == mj1.num || mj2.num - 2 == mj1.num)) return RemoveType.Chow;
+        return RemoveType.None;
     }
 }
